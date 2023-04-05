3edb60ca562b8e81526fa9099523e6acc9d78400
sigjig <sigmundklaa@outlook.com>
2023-04-05 19:28:26 +0200

    Add crc32 algorithm

--- zumo/include/utils/crc32.hh
+++ zumo/include/utils/crc32.hh
@@ -8,11 +8,28 @@
 namespace utils
 {
 
+/**
+ * @brief CRC32 algorithm, without using an expensive lookup table. Slower than
+ * with a table but more suited for MCUs with minimal RAM. Taken from:
+ * https://stackoverflow.com/questions/21001659/crc32-algorithm-implementation-in-c-without-a-look-up-table-and-with-a-public-li
+ *
+ * @param buf
+ * @param size
+ * @return uint32_t
+ */
 uint32_t
 crc32(const uint8_t* buf, size_t size)
 {
-    /* TODO: Implement */
-    return 0;
+    uint32_t crc = 0xFFFFFFFF;
+
+    for (uint_fast8_t i = 0; i < size && buf[i]; i++) {
+        crc ^= buf[i];
+        for (uint_fast8_t j = 7; j >= 0; j--) {
+            crc = (crc >> 1) ^ (0xEDB88320 & (-(crc & 1)));
+        }
+    }
+
+    return ~crc;
 }
 
 }; // namespace utils
