5e401631e98f813e3b9dff732e39194163ed095a
sigjig <sigmundklaa@outlook.com>
2023-04-09 18:17:14 +0200

    add reporting service

---
+++ zumo/include/report/report.hh
@@ -0,0 +1,53 @@
+/* The reporting service is reponsible for sending periodic status updates to
+ * the remote, with information regarding the device health and other desirable
+ * information*/
+
+#ifndef REPORT_HH__
+#define REPORT_HH__
+
+#include <Arduino.h>
+#include <io/io.hh>
+#include <utils/init.hh>
+
+namespace report
+{
+
+template <typename T> class reporter_
+{
+  protected:
+    io::sink& sink_;
+    uint32_t interval_us_;
+    uint64_t last_write_us_;
+
+    T buf_;
+
+    /**
+     * @brief Method responsible for generating reports. Should return the size
+     * of the generated report
+     *
+     * @return size_t
+     */
+    virtual size_t generate_() = 0;
+
+  public:
+    reporter_(io::sink& sink, uint32_t interval)
+        : sink_(sink), interval_us_(interval)
+    {
+    }
+
+    void
+    on_tick()
+    {
+        uint64_t tmp = micros();
+
+        if (tmp - last_write_us_ < interval_us_) {
+            return;
+        }
+
+        sink_.write(&buf_, generate_());
+        last_write_us_ = tmp;
+    }
+};
+}; // namespace report
+
+#endif // REPORT_HH__
